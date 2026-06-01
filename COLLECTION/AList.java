021a6cd4a769ec91d32b1397b499ec64dd62ae83
imrohitkr24 <rks9835070230@gmail.com>
2026-06-01 10:45:49 +0530

    Collection Basics (add,remove like all methods)

---
+++ COLLECTION/AList.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.util.ArrayList;
+public class AList{
+    public static void main(String[]args){
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(12);
+        System.out.println(list);
+        list.add(25);
+        list.add(20);
+        System.out.println(list);
+    }
+}
